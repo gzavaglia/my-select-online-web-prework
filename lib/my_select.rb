95698a936e847718506427b18a7f7c0963589f4f
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-27 19:25:34 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,18 @@
-def my_select(collection)
- vip = []
- i = 0 
- while i < collection.size
+# def my_select(collection)
+# vip = []
+# i = 0 
+# while i < collection.size
  
-# vip << yield(collection) if true 
-# i = i +1 
- end #end while 
- return vip 
-end #end my_select()
+# # vip << yield(collection) if true 
+# # i = i +1 
+# end #end while 
+# return vip 
+# end #end my_select()
+
+  def select(&block)
+    result =[]
+    self.each do |element|
+      result << element if block.call(element) == true
+    end
+    result
+  end
