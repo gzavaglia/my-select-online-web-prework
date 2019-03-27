c49d3919d14ee8138e7bac28e3f39a7ecebd8dcc
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-27 19:25:24 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,9 @@ def my_select(collection)
  vip = []
  i = 0 
  while i < collection.size
- vip << yield(collection) if true 
- i = i +1 
+ 
+# vip << yield(collection) if true 
+# i = i +1 
  end #end while 
  return vip 
 end #end my_select()
