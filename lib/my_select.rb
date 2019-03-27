b01b1493f4e738ffcd2f984d1dcff4edc827a601
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-27 19:40:58 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,8 +3,10 @@ vip = []
 i = 0 
 while i <collection.length 
   item = yield(collection[i])
-  vip.insert(i,item)
+  if item == true 
+  vip.insert(i,collection[i])
   i = i+1 
+end #end if 
 end #end while
   vip
 end #end my_select()
